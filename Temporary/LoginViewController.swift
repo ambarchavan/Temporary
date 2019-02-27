4bbd8aa6c42b234560a32b0a53ff71aec8541ac7
ambar <chavan.amber@siliconstack.com.au>
2019-02-27 15:36:20 +0530

    first commit

---
+++ Temporary/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  Temporary
+//
+//  Created by Macbook on 27/02/19.
+//  Copyright © 2019 SiliconStack. All rights reserved.
+//
+
+import UIKit
+
+public class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextfld: UITextField!
+    @IBOutlet weak var passwordTextFld: UITextField!
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("username \(String(describing: emailTextfld.text)) password \(String(describing: passwordTextFld.text))")
+    }
+    
+    public init() {
+        super.init(nibName: "LoginViewController", bundle: Bundle(for: LoginViewController.self))
+    }
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Temporary/Temporary.h
@@ -0,0 +1,19 @@
+//
+//  Temporary.h
+//  Temporary
+//
+//  Created by Macbook on 27/02/19.
+//  Copyright © 2019 SiliconStack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Temporary.
+FOUNDATION_EXPORT double TemporaryVersionNumber;
+
+//! Project version string for Temporary.
+FOUNDATION_EXPORT const unsigned char TemporaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Temporary/PublicHeader.h>
+
+
